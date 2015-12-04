e1a800b5013ff887bee72b12669d18eb9b969c88
kozue_noro <kozuenoro1207@gmail.com>
2015-12-04 04:52:19 +0000

    [add]user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
